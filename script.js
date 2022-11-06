4836ef0803665893e6959b990bf0ef234ff3ff49
Jay <jay.webp@gmail.com>
2022-11-06 13:35:26 +1100

    Implement expandable sections

--- script.js
+++ script.js
@@ -1,51 +1,54 @@
-// EXPANDABLE SECTION LOGIC
-// get reference to .expandable buttons
-// add event listeners ( 'click', toggleDisplay() ) to all 
-// default: button inactive, content display:none;
-// when button is clicked, toggle "active" and add .active
-// when button is active, expand the "next sibling" section, .content
-// when button is inactive, hide content (content display: none)
+document.addEventListener("DOMContentLoaded", function () {
+    // EXPANDABLE SECTION LOGIC
+    // get reference to .expandable buttons
+    // add event listeners ( 'click', toggleDisplay() ) to all 
+    // default: button inactive, content display:none;
+    // when button is clicked, toggle "active" and add .active
+    // when button is active, expand the "next sibling" section, .content
+    // when button is inactive, hide content (content display: none)
+    const expandables = document.querySelectorAll('.expandable');
 
-//LIGHT MODE/DARK MODE
-// when #toggle-theme button is pressed
-// if currently light, add .dark to root
-// if dark, remove .dark from root
-// I wonder if I can add a nice fade in effect as well
-// add .current class to the corresponding <i></i>, #light or #dark, and remove it from the other
+    expandables.forEach(element => {
+        element.addEventListener("click", () => {
+            element.classList.toggle('active');
 
-// also, add .current to the currently selected theme on load
+            const content = element.nextElementSibling;
 
-const themeButton = document.querySelector('#toggle-theme');
-const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
-const currentTheme = localStorage.getItem("theme");
+            content.style.maxHeight
+                ? content.style.maxHeight = null
+                : content.style.maxHeight = content.scrollHeight + 'px';
+        })
+    });
 
-// Based off browser preference, toggling will add the stored theme
-// Which either changes from the default or has no effect
-if (currentTheme === "dark") {
-    document.body.classList.toggle("dark");
-} else if (currentTheme === "light") {
-    document.body.classList.toggle("light");
-}
+    const themeButton = document.querySelector('#toggle-theme');
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    const currentTheme = localStorage.getItem('theme');
 
-themeButton.addEventListener("click", function() {
-    let theme;
+    // Based off browser preference, toggling will add the stored theme
+    // Which either changes from the default or has no effect
+    if (currentTheme === 'dark') {
+        document.body.classList.toggle('dark');
+    } else if (currentTheme === 'light') {
+        document.body.classList.toggle('light');
+    }
 
-    if(prefersDarkMode.matches) {
-        // If the user prefers dark mode,
-        // toggle body.light
-        document.body.classList.toggle("light");
+    themeButton.addEventListener('click', function() {
+        let theme;
+        if(prefersDarkMode.matches) {
+            document.body.classList.toggle('light');
 
-        theme = // if .light is on body
-        document.body.classList.contains("light")
-        ? "light" // theme = light
-        : "dark"; // else, dark
-    } else {
-        document.body.classList.toggle("dark");
+            theme =
+            document.body.classList.contains('light')
+            ? 'light'
+            : 'dark';
+        } else {
+            document.body.classList.toggle('dark');
 
-        theme = 
-        document.body.classList.contains("dark")
-        ? "dark"
-        : "light";
-    } 
-    localStorage.setItem("theme", theme);
+            theme = 
+            document.body.classList.contains('dark')
+            ? 'dark'
+            : 'light';
+        } 
+        localStorage.setItem('theme', theme);
+    });
 });
